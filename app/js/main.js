a694940b9519ebc5676c8a68fbac03d133589884
igorgaev2014 <59573818+igorgaev2014@users.noreply.github.com>
2021-07-10 20:36:31 +0500

    change dist to docs

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
   $('.header__slider').slick({
     infinite: true,
-    fade: true,
+    fade: false,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="images"></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="images"></img>',    
     asNavFor: '.slider-dots',
