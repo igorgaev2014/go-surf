05aa1e9bdb95a313048f327545a971d5dd17caca
igorgaev2014 <igorgaev2014@gmail.com>
2021-08-01 20:34:14 +0300

    part 5 build

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,6 +12,10 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
+    responsive: [{
+      breakpoint: 961,
+      settings: "unslick"
+    }, ]
   });
 
   $('.surf-slider').slick({
@@ -20,6 +24,33 @@ $(function () {
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="images"></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="images"></img>',
     asNavFor: '.slider-map',
+    responsive: [{
+        breakpoint: 1210,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 426,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+        }
+      },
+    ]
   });
 
   $('.slider-map').slick({
@@ -27,7 +58,28 @@ $(function () {
     slidesToScroll: 1,
     arrows: false,
     asNavFor: '.surf-slider',
-    focusOnSelect: true
+    focusOnSelect: true,
+    responsive: [{
+        breakpoint: 1102,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        }
+      },
+    ]
   });
 
   $('.holder__slider, .shop__slider').slick({
@@ -82,6 +134,10 @@ $(function () {
 
   $('.surfboard-box__circle').on('click', function () {
     $(this).toggleClass('active')
+  });
+
+  $('.menu-btn').on('click', function () {
+    $('.menu').toggleClass('active');
   });
 
 });
